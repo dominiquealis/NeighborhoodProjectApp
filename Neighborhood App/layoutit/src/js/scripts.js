6c2e4608f51c9ef4aa937492a189436da4dba383
dominiquealis <dominique.ryan@gmail.com>
2016-12-03 16:49:22 -0500

    Creating Input Form

--- Neighborhood App/layoutit/src/js/scripts.js
+++ Neighborhood App/layoutit/src/js/scripts.js
@@ -1,1 +1,29 @@
-// Empty JS for your own code to be here
+// code to get push info to firebase
+
+$(document).ready(function(){
+console.log("loaded");
+function writeUserData(userId, name, email, imageUrl) {
+  firebase.database().ref('homes/' + streetno).set({
+    username: name,
+    email: email,
+    profile_picture : imageUrl
+  });
+}
+
+
+});
+
+
+
+// $("#submit").on('click', function(){
+// 	var email = $("#email").val();
+// 	var password = $("#pw").val();
+// 	 console.log('click');
+// firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // ...
+// });
+// })
+
