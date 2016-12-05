ca648eae01ef00a49c65e5ffaaf4a67d5eb9f582
dominiquealis <dominique.ryan@gmail.com>
2016-12-05 08:54:09 -0500

    Testing Firebase
    working to push data.

--- Neighborhood App/layoutit/src/js/scripts.js
+++ Neighborhood App/layoutit/src/js/scripts.js
@@ -2,15 +2,35 @@
 
 $(document).ready(function(){
 console.log("loaded");
-function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('homes/' + streetno).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
+function writeUserData() {
+  firebase.database().ref('homes/' + 708).set({
+      "persons" : {
+        "person1" : {
+          "firstname" : "Dominique",
+          "lastname" : "Ryan"
+        },
+        "person2" : {
+          "firstname" : "Andrew",
+          "lastname" : "Nichols"
+        }
+      },
+      "pets" : {
+        "cats" : {
+          "cat1" : "Binx",
+          "cat2" : "Miami",
+          "cat3" : "Zelda"
+        }
+      },
+      "phones" : {
+        "phone1" : "222-222-2222",
+        "phone2" : "333-333-3333"
+      },
+      "streetaddress" : {
+        "address" : "712 Ritchie Ave"
+      }
   });
 }
-
-
+writeUserData()
 });
 
 
